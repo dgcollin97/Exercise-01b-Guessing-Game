06b05aec5dc1b896754248a2992165ec269282f1
dgcollin97 <70244551+dgcollin97@users.noreply.github.com>
2020-08-28 18:37:57 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,2 +1,10 @@
 import sys, random
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+hundred = 300
+guess = input("Guess a hundred from 100 to 900: ")
+guess = int(guess)
+if guess == hundred:
+    print("Great job! You got it!")
+else:
+    print("Sorry, better luck next time.")
+    print("The hundred was " + str(hundred))
